d07c178faea8b170cddc57211db16410b3dadb63
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-23 20:16:06 -0600

    profile controller

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,16 +1,32 @@
 class Api::ProfilesController < ApplicationController
   def index
-  end
-
-  def show
+    render json: Profile.all
   end
 
   def create
+    profile = Profile.new(profile_params)
+    if porfile.save
+      render json: profile
+    else
+      render json: { errors: profile.errors }, status: :unprocessable_entity 
+    end
   end
 
   def update
+    profile = Profile.find(params[:id])
+    profile.update(complete: !profile.complete)
+    render json: profile
   end
 
   def destroy
+    Profile.find(params[:id]).destroy
+    render json: { message: 'profile deleted' }
   end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :description, :body, :email, :phone)
+  end
+
 end
