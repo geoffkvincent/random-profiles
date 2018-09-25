60726024b523b955dc90f1f819b15a16e68d16d2
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-24 19:47:55 -0600

    get reducer done

--- client/src/reducers/profiles.js
+++ client/src/reducers/profiles.js
@@ -7,6 +7,15 @@ const DELETE_PROFILES = 'DELETE_PROFILES'
 export const getProfiles = () => {
   return (dispatch) => {
     axios.get('/api/profiles')
-      .then(res => dispatch)
+      .then(res => dispatch({ type: PROFILES, profiles: res.data}) )
+  }
+}
+
+export default (state = [], action ) => {
+  switch(action.type) {
+    case PROFILES:
+      return action.apps
+    default:
+      return state;
   }
 }
