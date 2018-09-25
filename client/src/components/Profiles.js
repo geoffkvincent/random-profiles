4a3cf16068728172db13fa2881fdbe8f59c26070
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-24 21:54:24 -0600

    profiles component done

--- client/src/components/Profiles.js
+++ client/src/components/Profiles.js
@@ -1,1 +1,55 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { getProfiles } from '../reducers/profiles'
+import {} from 'semantic-ui-react'
+
+class Profiles extends React.Component {
+  
+  componentDidMount() {
+    this.props.dispatch(getProfiles())
+  }
+
+  profiles = () => {
+    const profiles = this.props
+    return profiles.map(profile => 
+      <Card key={profile.id}>
+        <Card.Content>
+          <Card.Header>
+            {profile.name}
+          </Card.Header>
+          <Card.Meta>
+            <span>
+              {profile.description}
+            </span>
+          </Card.Meta>
+          <Card.Description>
+            {profile.name}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Link to={`/profiles/${profile.id}`}>
+            View Profile
+          </Link>
+        </Card.Content>
+      </Card>
+    )
+  }
+  
+  render() {
+    return (
+      <Container>
+        <Header as="h3" textAlign="center">Profiles</Header>
+          <Card.Group itemsPerRow={4}>
+           { this.profiles() }
+          </Card.Group>
+        </Container>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { profiles: state.profiles }
+}
+
+export default connect(mapStateToProps)(Profiles)
